a625faf4e454e2cc727334b3eabe9d76eef1037c
NikolayZhukovsky <157972351+NikolayZhukovsky@users.noreply.github.com>
2026-04-01 19:03:37 +0300

    Update MC_main.py

--- MC_main.py
+++ MC_main.py
@@ -18,7 +18,7 @@ matplotlib.rcParams['toolbar'] = 'None'
 
 
 class MplCanvas(FigureCanvas):
-    def __init__(self, width=6, height=4, dpi=100):
+    def __init__(self, parent=None, width=6, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         super().__init__(fig)
